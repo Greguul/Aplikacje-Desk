53bc1ccd4df62d3cef671e4128fd7310852cdb94
olek <olek.h.kielce@gmail.com>
2022-09-29 13:17:45 +0200

    d

---
+++ PassingParametersToMethod/TestingParamteters.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PassingParametersToMethod
+{
+    class TestingParamteters
+    {
+        public void Test1()
+        {
+            byte number = 15;
+            Console.WriteLine("Zmienna number = " + number);
+
+            ChangeValuev2(ref number);
+            Console.WriteLine("Zmienna number = " + number);
+
+            int random = GetRandomNumber();
+
+            GetRandomNumber(out random);
+
+            Console.WriteLine("Liczba losowa =" + random);
+        }
+
+         public void changevalue(byte value)
+        {
+            Console.WriteLine("Parametr value = " +value);
+            value = (byte)(value + 10);
+            Console.WriteLine("Parametr value = " + value);
+
+        }
+
+        private void ChangeValuev2 (ref byte value)
+        {
+            Console.WriteLine("Parametr value =" + value);
+            value = (byte)(value + 10);
+            Console.WriteLine("Parametr value =" + value);
+        }
+
+        private int GetRandomNumber()
+        {
+            return new Random().Next(); 
+        }
+
+
+        private void GetRandomNumber (out int value)
+        {
+            value = new Random().Next();
+        }
+    }
+
+
+}
